c285f5d702450b1a845ebf979487b91d4fed2524
Phil Dangerfield <philipdangerfield@Philips-MBP.askey.com>
2023-05-13 15:13:27 -0600

    adjusted add a role so that you can just select the department name from a list and it'll grab the departments id and enter that in so the user doesn't need to know that.

--- server.js
+++ server.js
@@ -35,20 +35,29 @@ function mainMenu() {
 
     .then((answers) => {
       if (answers.action === "View all Departments") {
-        db.query("SELECT * FROM department;", function (err, results) {
-          console.table(results);
-          mainMenu();
-        });
+        db.query(
+          "SELECT id as deparment_id, name as department_name FROM department;",
+          function (err, results) {
+            console.table(results);
+            mainMenu();
+          }
+        );
       } else if (answers.action === "View all Roles") {
-        db.query("SELECT * FROM role;", function (err, results) {
-          console.table(results);
-          mainMenu();
-        });
+        db.query(
+          "SELECT role.title as job_title, role.id as role_id, department.name as department_name, role.salary FROM role JOIN department ON role.department_id = department.id;",
+          function (err, results) {
+            console.table(results);
+            mainMenu();
+          }
+        );
       } else if (answers.action === "View all Employees") {
-        db.query("SELECT * FROM employee;", function (err, results) {
-          console.table(results);
-          mainMenu();
-        });
+        db.query(
+          "SELECT employee.id as employee_id, employee.first_name as first_name, employee.last_name as last_name, role.title as job_title, department.name as department_name, role.salary as salary, manager.first_name as manager_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee as manager ON employee.manager_id = manager.id;",
+          function (err, results) {
+            console.table(results);
+            mainMenu();
+          }
+        );
       } else if (answers.action === "Add a Department") {
         inquirer
           .prompt([
@@ -83,21 +92,47 @@ function mainMenu() {
               message: "What is the salary?",
             },
             {
-              type: "input",
-              name: "department_id",
+              type: "list",
+              name: "department_name",
               message: "What department is this role for?",
+              choices: function () {
+                return new Promise(function (resolve, reject) {
+                  db.query(
+                    "SELECT name FROM department",
+                    function (err, results) {
+                      if (err) {
+                        reject(err);
+                      } else {
+                        const departmentNames = results.map((row) => row.name);
+                        resolve(departmentNames);
+                      }
+                    }
+                  );
+                });
+              },
             },
           ])
           .then((answers) => {
             db.query(
-              `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
-              [answers.title, answers.salary, answers.department_id],
+              "SELECT id FROM department WHERE name = ?",
+              [answers.department_name],
               function (err, results) {
                 if (err) {
                   console.log(err);
                 } else {
-                  console.log(`${answers.title} has been added to roles`);
-                  mainMenu();
+                  const department_id = results[0].id;
+                  db.query(
+                    `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+                    [answers.title, answers.salary, department_id],
+                    function (err, results) {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log(`${answers.title} has been added to roles`);
+                        mainMenu();
+                      }
+                    }
+                  );
                 }
               }
             );
