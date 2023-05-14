119fc1a8359b57bdf398e034c4ebc78fb973ea69
Phil Dangerfield <philipdangerfield@Philips-MBP.askey.com>
2023-05-14 15:11:55 -0600

    added update an employee role and added add the manager when adding an employee

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ function mainMenu() {
           "Add a Role",
           "Add an Employee",
           "Update an Employee Role",
+          "Exit",
         ],
       },
     ])
@@ -52,7 +53,7 @@ function mainMenu() {
         );
       } else if (answers.action === "View all Employees") {
         db.query(
-          "SELECT employee.id as employee_id, employee.first_name as first_name, employee.last_name as last_name, role.title as job_title, department.name as department_name, role.salary as salary, manager.first_name as manager_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee as manager ON employee.manager_id = manager.id;",
+          "SELECT employee.id as employee_id, employee.first_name as first_name, employee.last_name as last_name, role.title as job_title, department.name as department_name, role.salary as salary, CONCAT(manager.first_name, ' ', manager.last_name) as manager_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee as manager ON employee.manager_id = manager.id;",
           function (err, results) {
             console.table(results);
             mainMenu();
@@ -138,6 +139,7 @@ function mainMenu() {
             );
           });
       } else if (answers.action === "Add an Employee") {
+        let managerName = '';
         inquirer
           .prompt([
             {
@@ -146,32 +148,47 @@ function mainMenu() {
               message: "What is the first name of the Employee?",
             },
             {
-                type: "input",
-                name: "last_name",
-                message: "What is the last name of the Employee?",
-              },
+              type: "input",
+              name: "last_name",
+              message: "What is the last name of the Employee?",
+            },
             {
               type: "list",
               name: "title",
               message: "What is the Employee's title?",
               choices: function () {
                 return new Promise(function (resolve, reject) {
-                  db.query(
-                    "SELECT title FROM role",
-                    function (err, results) {
-                      if (err) {
-                        reject(err);
-                      } else {
-                        const roleTitles = results.map((row) => row.title);
-                        resolve(roleTitles);
-                      }
+                  db.query("SELECT title FROM role", function (err, results) {
+                    if (err) {
+                      reject(err);
+                    } else {
+                      const roleTitles = results.map((row) => row.title);
+                      resolve(roleTitles);
                     }
-                  );
+                  });
                 });
               },
             },
+            {
+                type: "list",
+                name: "manager",
+                message: "Who is the Employee's manager?",
+                choices: function () {
+                    return new Promise(function (resolve, reject) {
+                        db.query("SELECT CONCAT(first_name, ' ', last_name) AS manager_name, id FROM employee", function (err, results) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                           const managerNames = results.map((row) => row.manager_name);
+                            resolve(managerNames);
+                        }
+                        });
+                    });
+                    }
+            }
           ])
           .then((answers) => {
+            managerName = answers.manager;
             db.query(
               "SELECT id FROM role WHERE title = ?",
               [answers.title],
@@ -180,27 +197,96 @@ function mainMenu() {
                   console.log(err);
                 } else {
                   const role_id = results[0].id;
+                  db.query('SELECT id FROM employee WHERE CONCAT(first_name, " ", last_name) = ?', [managerName], function(err, results) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        const manager_id = results[0].id;
                   db.query(
-                    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, null)`,
-                    [answers.first_name, answers.last_name, role_id],
+                    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+                    [answers.first_name, answers.last_name, role_id, manager_id],
                     function (err, results) {
                       if (err) {
                         console.log(err);
                       } else {
-                        console.log(`${answers.first_name} ${answers.last_name} has been added to employee's`);
+                        console.log(
+                          `${answers.first_name} ${answers.last_name} has been added to employee's`
+                        );
                         mainMenu();
-                      }
-                    }
-                  );
-                }
-              }
-            );
+
+                      } 
+                    } 
+                  ); 
+                } 
+              } 
+            ); 
+
+          }; 
+        } 
+        );
+        });
+          } else if (answers.action === "Update an Employee Role") {
+        inquirer
+          .prompt([
+    {
+      type: "list",
+      name: "Employee Name",
+      message: "What is the name of the employee you would like to update?",
+      choices: function() {
+        return new Promise(function(resolve, reject) {
+          db.query("SELECT first_name, last_name FROM employee", function(err, results) {
+            if (err) {
+              reject(err);
+            } else {
+              const employeeNames = results.map(row => row.first_name + " " + row.last_name);
+              resolve(employeeNames);
+            }
           });
-      } else if (answers.action === "Update an Employee Role") {
-        console.log("You chose to update an employee role");
-        ("UPDATE employee SET role_id = answers.role_id WHERE id = answers.id;");
+        });
       }
-    })
+    },
+    {
+      type: "list",
+      name: "title",
+      message: "What is the Employee's new title?",
+      choices: function() {
+        return new Promise(function(resolve, reject) {
+          db.query("SELECT title, id FROM role", function(err, results) {
+            if (err) {
+              reject(err);
+            } else {
+              const roleTitles = results.map(row => {
+                return {
+                  name: row.title,
+                  value: row.id
+                };
+              });
+              resolve(roleTitles);
+            }
+          });
+        });
+      }
+    }
+  ])
+  .then((answers) => {
+    db.query(
+      `UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?`,
+      [answers.title, answers["Employee Name"].split(" ")[0], answers["Employee Name"].split(" ")[1]],
+      function(err, results) {
+        if (err) {
+          console.log("Error updating employee's title:", err);
+        } else {
+          console.log(`${answers["Employee Name"]} title has been updated`);
+        }
+        mainMenu();
+      }
+    );
+  });
+        
+    } else if (answers.action === "Exit") {
+        db.end();
+        }
+    }) 
     .catch((error) => {
       if (error.isTtyError) {
         console.log("Prompt couldn't be rendered in the current environment");
